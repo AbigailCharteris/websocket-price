74821507b53ad3f9ec43bac103035ecba0b6c64e
AdrianCharteris <AdrianCharteris@Yahoo.co.uk>
2016-10-27 21:22:56 +0100

    promises added

--- partials/ticker/tickerCtrl.js
+++ partials/ticker/tickerCtrl.js
@@ -3,13 +3,20 @@ define(["require", "exports", "../../services/PriceStreamService"], function (re
     // import PriceTickerDirective from "../../services/price-ticker-directive";
     class TickerCtrl {
         //    constructor(private $scope: ng.IScope, $log: ng.ILogService, priceStreamService: PriceStreamService) {  // --> can't inject PriceStreamService!??!!
-        constructor($scope, $rootScope, $state, $log, $q, localStorageService) {
+        constructor($scope, $rootScope, $state, $log, $http, $q, localStorageService) {
             this.$scope = $scope;
             this.$rootScope = $rootScope;
             this.$state = $state;
+            this.$http = $http;
             this.$q = $q;
             this.localStorageService = localStorageService;
-            this.priceStreamingService = new PriceStreamService_1.default($scope, $rootScope, $state, $log, $q, localStorageService);
+            //this.priceStreamingService = new PriceStreamSvc($scope, $rootScope, $state, $log, $q, localStorageService);
+            this.StreamPrices("1").then(response => {
+                console.log(response);
+            });
+            this.StreamPricesChain("2").then(response => {
+                console.log(response);
+            });
         }
         static get fullName() {
             return "TickerCtrl";
@@ -19,22 +26,46 @@ define(["require", "exports", "../../services/PriceStreamService"], function (re
         }
         ;
         static Register(module) {
-            module.controller(TickerCtrl.fullName, ["$scope", "$rootScope", "$state", "$log", "$q", "angular.local.storage", PriceStreamService_1.default.fullName, TickerCtrl]);
+            module.controller(TickerCtrl.fullName, ["$scope", "$rootScope", "$state", "$log", "$http", "$q", "angular.local.storage", PriceStreamService_1.default.fullName, TickerCtrl]);
             //module.controller(TickerCtrl.fullName, []);
         }
-        GetPrices() {
-            this.price = this.priceStreamingService.latestPrice;
-            this.priceStreamingService.StreamPrices()
-                .then(data => {
-                this.price = String(data);
-                this.price = this.priceStreamingService.latestPrice;
+        // public GetPrices() {
+        //     this.price = this.priceStreamingService.latestPrice;
+        //     this.priceStreamingService.StreamPrices()
+        //         .then(data => {
+        //             this.price = String(data);
+        //             this.price = this.priceStreamingService.latestPrice;
+        //         });
+        //     //this.$scope.$on(this.priceStreamingService.latestPrice, (data) => {this.price = String(data); });
+        // }
+        // public StopStreaming() {
+        //     this.priceHistory.push(this.price);
+        //     this.priceStreamingService.StopStream();
+        //     this.priceStreamingService.StorePriceHistory(this.priceHistory);
+        // }
+        StreamPrices(id) {
+            let defer = this.$q.defer();
+            let uri = "http://localhost:51633/api/products";
+            let promise = this.$http.get(uri + "/" + id);
+            promise.success(response => {
+                defer.resolve({ response });
             });
-            //this.$scope.$on(this.priceStreamingService.latestPrice, (data) => {this.price = String(data); });
+            promise.error(error => {
+                defer.reject(error);
+            });
+            return defer.promise;
         }
-        StopStreaming() {
-            this.priceHistory.push(this.price);
-            this.priceStreamingService.StopStream();
-            this.priceStreamingService.StorePriceHistory(this.priceHistory);
+        StreamPricesChain(id) {
+            let q = this.$q.defer();
+            let uri = "http://localhost:51633/api/products";
+            this.$http.get(uri + "/" + id)
+                .then((response) => {
+                return { data: response.data };
+            }, (httpError) => {
+                // translate the error
+                throw httpError.status + " : " + httpError.data;
+            });
+            return q.promise;
         }
     }
     Object.defineProperty(exports, "__esModule", { value: true });
--- partials/ticker/tickerCtrl.ts
+++ partials/ticker/tickerCtrl.ts
@@ -22,36 +22,125 @@ export default class TickerCtrl {
     };
 
     static Register(module: ng.IModule) {
-        module.controller(TickerCtrl.fullName, ["$scope", "$rootScope", "$state", "$log", "$q", "angular.local.storage", PriceStreamSvc.fullName, TickerCtrl]);  
+        module.controller(TickerCtrl.fullName, ["$scope", "$rootScope", "$state", "$log", "$http", "$q", "angular.local.storage", PriceStreamSvc.fullName, TickerCtrl]);
         //module.controller(TickerCtrl.fullName, []);
     }
 
-//    constructor(private $scope: ng.IScope, $log: ng.ILogService, priceStreamService: PriceStreamService) {  // --> can't inject PriceStreamService!??!!
+    //    constructor(private $scope: ng.IScope, $log: ng.ILogService, priceStreamService: PriceStreamService) {  // --> can't inject PriceStreamService!??!!
     constructor(public $scope: ng.IScope, private $rootScope: ng.IRootScopeService,
-                private $state: ng.ui.IStateService, $log: ng.ILogService,
-                private $q: ng.IQService, private localStorageService: ng.local.storage.ILocalStorageService){//, private priceStreamService: PriceStreamSvc) {
+        private $state: ng.ui.IStateService, $log: ng.ILogService, private $http: ng.IHttpService,
+        private $q: ng.IQService, private localStorageService: ng.local.storage.ILocalStorageService) {//, private priceStreamService: PriceStreamSvc) {
 
 
-            this.priceStreamingService = new PriceStreamSvc($scope, $rootScope, $state, $log, $q, localStorageService);
+        //this.priceStreamingService = new PriceStreamSvc($scope, $rootScope, $state, $log, $q, localStorageService);
+        this.StreamPrices("1").then(response => {
+            console.log(response);
+        });
+
+        this.StreamPricesChain("2").then(response => {
+            console.log(response);
+        })
     }
 
-    public GetPrices() {
+    // public GetPrices() {
 
-        this.price = this.priceStreamingService.latestPrice;
+    //     this.price = this.priceStreamingService.latestPrice;
 
-        this.priceStreamingService.StreamPrices()
-            .then(data => {
-                this.price = String(data);
-                this.price = this.priceStreamingService.latestPrice;
-            });
+    //     this.priceStreamingService.StreamPrices()
+    //         .then(data => {
+    //             this.price = String(data);
+    //             this.price = this.priceStreamingService.latestPrice;
+    //         });
 
-        //this.$scope.$on(this.priceStreamingService.latestPrice, (data) => {this.price = String(data); });
+    //     //this.$scope.$on(this.priceStreamingService.latestPrice, (data) => {this.price = String(data); });
+    // }
+
+    // public StopStreaming() {
+    //     this.priceHistory.push(this.price);
+    //     this.priceStreamingService.StopStream();
+    //     this.priceStreamingService.StorePriceHistory(this.priceHistory);
+    // }
+
+    public StreamPrices(id: string): ng.IPromise<{}> {
+
+        let defer = this.$q.defer();
+        let uri = "http://localhost:51633/api/products";
+
+        let promise = this.$http.get(uri + "/" + id);
+
+        promise.success(response =>{
+            defer.resolve({response});
+        });
+
+        promise.error(error => {
+            defer.reject(error);
+        });
+
+        return defer.promise;
     }
 
-    public StopStreaming() {
-        this.priceHistory.push(this.price);
-        this.priceStreamingService.StopStream();
-        this.priceStreamingService.StorePriceHistory(this.priceHistory);
+    public StreamPricesChain(id: string): ng.IHttpPromise<{}> {
+
+        let q = this.$q.defer();
+        let uri = "http://localhost:51633/api/products";
+
+        this.$http.get(uri + "/" + id)
+                  .then((response) => {
+                            return { data: response.data };
+                        },
+                        (httpError) => {
+                            // translate the error
+                            throw httpError.status + " : " + httpError.data;
+                        });
+
+        return q.promise;
     }
+
+
+    // handle error and transform data
+    // this.getMovie = function(movie) {
+    //     return $http.get('/api/v1/movies/' + movie)
+    //            .then(
+    //               function (response) {
+    //                 return {
+    //                    title: response.data.title,
+    //                    cost:  response.data.price
+    //                 });
+    //               },
+    //               function (httpError) {
+    //                  // translate the error
+    //                  throw httpError.status + " : " + 
+    //                        httpError.data;
+    //               });
+    // };
+
+    // Chained promises for multiple calls
+    // service('asyncService', function($http, $q) {
+    //     return {
+    //         loadDataFromUrls: function (urls) {
+    //             var deferred = $q.defer();
+    //             var urlCalls = [];
+    //             angular.forEach(urls, function (url) {
+    //                 urlCalls.push($http.get(url.url));
+    //             });
+    //             // they may, in fact, all be done, but this
+    //             // executes the callbacks in then, once they are
+    //             // completely finished.
+    //             $q.all(urlCalls)
+    //                 .then(
+    //                 function (results) {
+    //                     deferred.resolve(
+    //                         JSON.stringify(results))
+    //                 },
+    //                 function (errors) {
+    //                     deferred.reject(errors);
+    //                 },
+    //                 function (updates) {
+    //                     deferred.update(updates);
+    //                 });
+    //             return deferred.promise;
+    //         }
+    //     };
+    // });
 
 };
